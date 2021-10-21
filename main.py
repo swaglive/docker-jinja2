5655ac67b637658b6598738d137f89842047a1e4
Johnny Chang <66932108+hiimdoublej-swag@users.noreply.github.com>
2021-10-21 18:48:59 +0800

    Update github workflow, read from file

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import jinja2
 def render():
     # Construct Template object from env var
     # Render to output file
-    input = os.environ['RENDER__INPUT']
+    input_file = os.environ['RENDER__INPUT_FILE']
     output_file = os.environ['RENDER__OUTPUT_FILE']
     env = jinja2.Environment()
-    template = env.from_string(input)
+    with open(input_file, 'r') as fp:
+        template = env.from_string(fp.read())
     content = template.render({'env': os.environ})
     with open(output_file, 'w') as fp:
         fp.write(content)
